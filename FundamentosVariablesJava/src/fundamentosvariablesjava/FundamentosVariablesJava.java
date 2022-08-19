252c2ef4d7c66d3899ceddb7d5565fedb35b1fea
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-19 12:10:06 -0500

    Tipos Enteros y tipo String en Java

---
+++ FundamentosVariablesJava/src/fundamentosvariablesjava/FundamentosVariablesJava.java
@@ -0,0 +1,23 @@
+
+package fundamentosvariablesjava;
+
+public class FundamentosVariablesJava {
+
+    public static void main(String[] args) {
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera); // 10
+        
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera); // 5
+        
+        // String: No es un tipo primitivo, sino una clase en Java
+        // por eso va en mayúscula
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena); // Saludos
+     
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena); // Adios
+        
+    }    
+}
