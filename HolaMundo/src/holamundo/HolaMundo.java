d1d6fac4aa2cfd7947cbb2d44ee4230474425649
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-19 11:21:25 -0500

    HolaMundo en Java

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+// Mi clase en Java
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+    }
+    
+}
