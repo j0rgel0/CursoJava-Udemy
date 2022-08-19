dc8c63358788a5c1c904dc0f2a696098db7ebd06
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-19 18:39:59 -0500

    Tipo char en Java

---
+++ FundamentosVariablesJava/src/fundamentosvariablesjava/TipoChar.java
@@ -0,0 +1,27 @@
+package fundamentosvariablesjava;
+
+public class TipoChar {
+
+    public static void main(String[] args) {
+        char miCaracter = 'a';
+        System.out.println("miCaracter = " + miCaracter); // miCaracter = a
+
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);// varChar = !
+        
+        var varChar2 = '\u0021';
+        System.out.println("varChar2 = " + varChar2); //varChar2 = !
+
+        char varDecimal = 33;
+        System.out.println("varDecimal = " + varDecimal); //varDecimal = !
+        
+        var varDecimal2 = 33;
+        System.out.println("varDecimal2 = " + varDecimal2); // varDecimal2 = 33
+
+        char varCharsimbolo = '!';
+        System.out.println("varCharsimbolo = " + varCharsimbolo); //varCharsimbolo = !
+        
+        var varCharsimbolo2 = '!';
+        System.out.println("varCharsimbolo2 = " + varCharsimbolo2); //varCharsimbolo2 = !
+    }
+}
