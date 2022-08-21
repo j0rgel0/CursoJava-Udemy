b29facfa3f2155ff1ce1d0ef0d1d8e6c1fcfd348
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-20 22:54:02 -0500

    Sentencias de Control en Java

---
+++ SentenciasControl/src/ControlIfElse.java
@@ -0,0 +1,10 @@
+public class ControlIfElse {
+    public static void main(String[] args){
+        var condicion = true;
+        if (condicion) {
+            System.out.println("Condición verdadera"); //Condición verdadera
+        } else {
+            System.out.println("Condición falsa");
+        }
+    }
+}
