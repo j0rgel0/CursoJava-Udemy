514973797270a3035318a19b515ed55bfe820637
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-22 14:58:58 -0500

    Ciclo For en Java

---
+++ Ciclos/src/CicloFor.java
@@ -0,0 +1,12 @@
+public class CicloFor {
+    public static void main(String[] args){
+        for(var contador = 0; contador < 3; contador++){
+            System.out.println("contador = " + contador);
+        }
+        /*
+            contador = 0
+            contador = 1
+            contador = 2
+        */
+    }
+}
